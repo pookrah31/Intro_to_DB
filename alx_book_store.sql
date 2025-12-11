f9fc97bd98f74601d821d99fc53853a428963978
Pookrah31 <penielokrah31@gmail.com>
2025-12-11 17:53:29 +0000

    Add alx_book_store.sql with database schema

---
+++ alx_book_store.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE alx_book_store;
+
+USE alx_book_store;
+
+CREATE TABLE BOOKS(
+book_id INT PRIMARY KEY,
+title VARCHAR(130),
+author_id INT,
+price DOUBLE,
+publication_date DATE,
+FOREIGN KEY (author_id) REFERENCES AUTHORS(author_id)
+);
+
+CREATE TABLE AUTHORS(
+author_id INT PRIMARY KEY,
+author_name VARCHAR(215)
+);
+
+CREATE TABLE CUSTOMERS(
+customer_id INT PRIMARY KEY,
+customer_name VARCHAR(215),
+email VARCHAR(215),
+address TEXT
+);
+
+
+CREATE TABLE ORDERS(
+order_id INT PRIMARY KEY,
+customer_id INT,
+order_date DATE,
+FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
+);
+
+CREATE TABLE ORDER_DETAILS(
+orderdetailid INT PRIMARY KEY,
+order_id INT,
+book_id INT,
+quantity DOUBLE,
+FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
+FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+);
