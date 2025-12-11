c619e41560d67d12bdddd88a1b6c917e6e134d8f
Pookrah31 <penielokrah31@gmail.com>
2025-12-11 17:59:26 +0000

    Fix Authors table

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -2,23 +2,23 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 USE alx_book_store;
 
-CREATE TABLE AUTHORS(
+CREATE TABLE Authors(
 author_id INT PRIMARY KEY,
 author_name VARCHAR(215)
 );
 
-CREATE TABLE BOOKS(
+CREATE TABLE Books(
 book_id INT PRIMARY KEY,
 title VARCHAR(130),
 author_id INT,
 price DOUBLE,
 publication_date DATE,
-FOREIGN KEY (author_id) REFERENCES AUTHORS(author_id)
+FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
 
 
-CREATE TABLE CUSTOMERS(
+CREATE TABLE Customers(
 customer_id INT PRIMARY KEY,
 customer_name VARCHAR(215),
 email VARCHAR(215),
@@ -26,18 +26,18 @@ address TEXT
 );
 
 
-CREATE TABLE ORDERS(
+CREATE TABLE Orders(
 order_id INT PRIMARY KEY,
 customer_id INT,
 order_date DATE,
-FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
+FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE ORDER_DETAILS(
+CREATE TABLE Order_details(
 orderdetailid INT PRIMARY KEY,
 order_id INT,
 book_id INT,
 quantity DOUBLE,
-FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
-FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
